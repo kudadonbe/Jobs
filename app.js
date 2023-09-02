f4f86bec5bc5f783fb190d4964a2c7f91cef48eb
Hussain Shareef <hussain.shareef@gmail.com>
2023-09-02 15:44:22 +0500

    updates

---
+++ app.js
@@ -0,0 +1,169 @@
+/*
+
+Jobs applied
+
+Iulaan Number         | Job Title                               | Unit/Section
+(IUL)226-B/226/2023/5 | J-378782-02 Assistant Council Executive | Infromation Technology and Media Unit
+(IUL)226-B/226/2023/6 | J-378726-04 Assistant Council Executive | Corporate Affairs Section
+(IUL)226-B/226/2023/7 | J-378776-05 Assistant Council Executive | Public Service Unit
+
+
+
+
+
+
+
+
+
+
+
+
+Hussain Shareef
+A147829
+Hadhuvareege, HDh. Vaikaradhoo
+hussain.shareef@gmail.com, (+960) 9898250
+
+Practical Experiences
+2021 - Present	Assistant Council Officer
+THILADHUNMATHEE DHEKUNU BUREE VAIKARADHOO COUNCIL GE IDHAARAA
+
+2017 - 2021	Officer, Administrative [Projects Implementation Dept.]
+MALDIVES AIRPORTS COMPANY LIMITED, Hulhulé, Republic of Maldives.
+Main Project:
+Expansion & Upgradation of Velana International Airport. (Runway Project & New Passenger Terminal Building)
+Other Projects:
+New CIP/VIP Building, New Sea Plane Terminal, New Cargo Terminal Complex, New Fuel Farm and New Fuel Hydrant System
+
+2014 - 2017	Aircraft Interior Cleaner
+MALDIVES AIRPORTS COMPANY LIMITED, Hulhulé, Republic of Maldives.
+
+2012 - 2013	Operations Coordinator
+WATCO MALDIVES PVT, Malé, Republic of Maldives.
+
+2010 - 2011	Administrative Officer
+COLLAGE OF ISLAMIC STUDIES (كلية الدراسات الإسلامية), Malé, Republic of Maldives.
+
+2008 - 2009	Computer Technician Trainee
+HDH. ATOLL SCHOOL, HDh. Vaikaradhoo, Republic of Maldives.
+
+Secretary
+THILADHUNMATHEE DHEKUNUBUREE VAIKARADHOO COUNCLIGE IDAARAA
+HDh. Vaikaradhoo, Republic of Maldives.
+
+2005 - 2007	Assistant Finance Officer
+MALDIVES PORTS LIMITED, Malé, Republic of Maldives.
+
+Teller
+INDIRA GANDHI MEMORIAL HOSPITAL, Malé, Republic of Maldives.
+
+Cashier
+MEGA WAREHOUSE, Malé, Republic of Maldives.
+
+Sales Assistant
+HAGEEGEE BODU, Malé, Republic of Maldives. 
+
+Professional Training
+2014 - 2015	The Internet of Everything, webinar series
+CISCO NETWORKING ACADEMY.
+
+General Aviation Security Awareness Training
+AVIATION SECURITY COMMAND.
+
+2012 - 2013	Basic Fire Safety Course
+MALDIVES AIRPORTS COMPANY LIMITED.
+
+Basic Airside Safety Training
+MALDIVES AIRPORTS COMPANY LIMITED.
+
+2009 - 2010	الدورة التدريبية لمعلمي اللغة العربية والثقافية الإسلامي
+ISLAMIC UNIVERSITY OF MADHINA.
+  
+Education
+2014 - present	MALDIVES NATIONAL UNIVERSITY, Malé Republic of Maldives.
+Bachelors of Information Technology.
+
+Distinction in Discrete Mathematics.
+
+2013 - 2014	MALDIVES NATIONAL UNIVERSITY, Malé Republic of Maldives.
+Advanced Certificate in Information Technology.
+
+High Distinction in Fundamentals of Programming.
+High Distinction in Application Development. 
+Distinction in Computer Concepts and PC Application
+Distinction in English for further Studies.
+
+1994 - 2004	AHMADIYYA INTERNATIONAL SCHOOL, Malé Republic of Maldives.
+IGCSE, GCE and SSC
+Principles of Accounts 	A	Islamic Studies 	B
+Commerce 	A	Economics	C
+Fisheries Science	B	Dhivehi Language 	C
+Mathematics	B		
+.
+Additional skills
+	Software Development, Graphic Designing, Writer, fluent in Arabic keyboard, drawing 3D images and floor plans.
+
+Driving License: A0, A1, B1, C1
+
+References
+	Mohamed Rabeeu, Council Executive of Thiladhunmathee Dhekunu Buree Vaikaradhoo Council Ge Idhaaraa
++960 99004522, mohamed.rabeeu@vaikaradhoo.gov.mv
+
+Hawwa Latheef, Vice rector for the Academic of Collage of Islamic Studies
+(+960) 7907701, hawwalathyf@gmail.com
+
+
+
+
+
+[Slide 1: Title]
+[Company Logo]
+Presentation for Job Interview: Demonstrating Qualifications and Suitability for Assistant Council Executive Positions
+HDh. Atoll Council
+
+[Slide 2: Introduction]
+Good [morning/afternoon], esteemed members of the hiring committee. Thank you for the opportunity to present myself today. My name is Hussain Shareef, and I have applied for the Assistant Council Executive positions at HDh. Atoll Council. I am excited about the prospect of contributing to the council's success and growth in the roles of Assistant Council Executive in the Information Technology and Media Unit, Corporate Affairs Section, and Public Service Unit.
+
+[Slide 3: Professional Experience]
+Allow me to provide you with an overview of my practical experiences in the job field. Presently, I serve as an Assistant Council Officer at THILADHUNMATHEE DHEKUNU BUREE VAIKARADHOO COUNCIL GE IDHAARAA, a role that has allowed me to develop a comprehensive understanding of the council's functions and operations.
+
+[Slide 4: IT and Media Unit]
+In my application for the Assistant Council Executive position in the Information Technology and Media Unit, my educational background in Information Technology, coupled with my experience in various IT projects during my tenure at MALDIVES AIRPORTS COMPANY LIMITED and as a Computer Technician Trainee at HDH. Atoll School, makes me well-suited to lead and oversee the IT and media functions at HDh. Atoll Council.
+
+[Slide 5: Corporate Affairs Section]
+Regarding the Assistant Council Executive position in the Corporate Affairs Section, my background as an Administrative Officer and my proficiency in areas such as Administrative Management and Operations Coordination make me a strong fit for effectively managing corporate affairs and enhancing organizational processes.
+
+[Slide 6: Public Service Unit]
+As for the Assistant Council Executive position in the Public Service Unit, my experience in project implementation, coupled with my background as an Administrative Officer, qualifies me to lead and contribute to the public service department's goals.
+
+[Slide 7: Former Vice Rector]
+Furthermore, I had the privilege of serving as the Vice Rector for the Islamic University of Maldives, where I gained valuable leadership and management experience, fostering an environment of academic excellence and supporting the growth and development of the institution.
+
+[Slide 8: Skills and Training]
+I have consistently pursued professional development and additional skills that align with the responsibilities of an Assistant Council Executive. My training in "The Internet of Everything" and General Aviation Security Awareness, along with my proficiency in Software Development and Graphic Designing, will enable me to bring a unique perspective to the roles.
+
+[Slide 9: References]
+Before concluding, I would like to reiterate my appreciation for this opportunity. If needed, you may contact my references:
+
+1. Mr. Mohamed Rabeeu, Council Executive of Thiladhunmathee Dhekunu Buree Vaikaradhoo Council Ge Idhaaraa.
+2. Ms. Hawwa Latheef, former Vice Rector for the Islamic University of Maldives.
+3. Mr. Ismail Ibrahim, Leading Consultant in Airport Extension Projects.
+4. Mr. Ali Ibrahim, from the Deen of Dauwa Faculty at the Islamic University of Maldives.
+5. Ms. Nadira, Lecturer at Maldives National University, who taught us multimedia.
+
+Please feel free to reach out to them to gain further insight into my qualifications and work ethic.
+
+[Slide 10: Conclusion]
+In conclusion, I am enthusiastic about the possibility of joining HDh. Atoll Council as an Assistant Council Executive in the Information Technology and Media Unit, Corporate Affairs Section, or Public Service Unit. I am confident in my ability to contribute positively to the organization's vision and objectives. Thank you for considering my application, and I am eager to discuss how I can be an asset to HDh. Atoll Council.
+
+[Slide 11: Questions]
+Now, I am more than happy to answer any questions you may have.
+
+
+
+
+
+
+
+
+
+*/
---
+++ sgJob.js
@@ -0,0 +1,191 @@
+/*
+
+The name of the company:
+Organizations I'm interested in,
+    Org: Island Councils
+    island: 
+        1. HDh. Nolhivaram
+        2. HDh. Hirimaradhoo
+        3. Sh. Kanditheem
+        4. N. Henbadhoo
+        5. GDg. Faresmaadhodaa
+The name of the position:
+    Secretory General (SG)
+The job description
+    All the works that should be done by the most senior employee of CS 
+    Run the Org according to 
+    1. Civil Service Law
+    2. 7/2010 Local Government Law
+    3. Public Finace Law and Rules
+
+The salary range
+MVR 15000 - 25000
+The location of the job
+Organizations I'm interested in,
+    Org: Island Councils
+    island: 
+        1. HDh. Nolhivaram
+        2. HDh. Hirimaradhoo
+        3. Sh. Kanditheem
+        4. N. Henbadhoo
+        5. GDg. Faresmaadhodaa
+The deadline for applications
+Aug 31, 2023
+
+Maldives National University
+CERTIFICATE
+awarded to
+Hussain Shareef
+for successfully completing the short course on
+Public Procurement
+
+conducted by the Maldives National University in
+collaboration with the Ministry of Finance
+
+from 19th to 30th June 2022.
+
+
+
+add my fb link and others 
+https://www.facebook.com/kudadonbe
+
+https://www.facebook.com/hawwa.latheef
+https://www.facebook.com/Rabeeeu
+*/
+
+
+/*
+
+ussain Shareef
+A147829
+Hadhuvareege, HDh. Vaikaradhoo
+hussain.shareef@gmail.com, (+960) 9898250
+
+Practical Experiences
+2021 - Present	Assistant Council Officer
+THILADHUNMATHEE DHEKUNU BUREE VAIKARADHOO COUNCIL GE IDHAARAA
+
+2017 - 2021	Officer, Administrative [Projects Implementation Dept.]
+MALDIVES AIRPORTS COMPANY LIMITED, Hulhulé, Republic of Maldives.
+Main Project:
+Expansion & Upgradation of Velana International Airport. (Runway Project & New Passenger Terminal Building)
+Other Projects:
+New CIP/VIP Building, New Sea Plane Terminal, New Cargo Terminal Complex, New Fuel Farm and New Fuel Hydrant System
+
+2014 - 2017	Aircraft Interior Cleaner
+MALDIVES AIRPORTS COMPANY LIMITED, Hulhulé, Republic of Maldives.
+
+2012 - 2013	Operations Coordinator
+WATCO MALDIVES PVT, Malé, Republic of Maldives.
+
+2010 - 2011	Administrative Officer
+COLLAGE OF ISLAMIC STUDIES (كلية الدراسات الإسلامية), Malé, Republic of Maldives.
+
+2008 - 2009	Computer Technician Trainee
+HDH. ATOLL SCHOOL, HDh. Vaikaradhoo, Republic of Maldives.
+
+Secretary
+THILADHUNMATHEE DHEKUNUBUREE VAIKARADHOO COUNCLIGE IDAARAA
+HDh. Vaikaradhoo, Republic of Maldives.
+
+2005 - 2007	Assistant Finance Officer
+MALDIVES PORTS LIMITED, Malé, Republic of Maldives.
+
+Teller
+INDIRA GANDHI MEMORIAL HOSPITAL, Malé, Republic of Maldives.
+
+Cashier
+MEGA WAREHOUSE, Malé, Republic of Maldives.
+
+Sales Assistant
+HAGEEGEE BODU, Malé, Republic of Maldives. 
+
+Professional Training
+2014 - 2015	The Internet of Everything, webinar series
+CISCO NETWORKING ACADEMY.
+
+General Aviation Security Awareness Training
+AVIATION SECURITY COMMAND.
+
+2012 - 2013	Basic Fire Safety Course
+MALDIVES AIRPORTS COMPANY LIMITED.
+
+Basic Airside Safety Training
+MALDIVES AIRPORTS COMPANY LIMITED.
+
+2009 - 2010	الدورة التدريبية لمعلمي اللغة العربية والثقافية الإسلامي
+ISLAMIC UNIVERSITY OF MADHINA.
+  
+Education
+2014 - present	MALDIVES NATIONAL UNIVERSITY, Malé Republic of Maldives.
+Bachelors of Information Technology.
+
+Distinction in Discrete Mathematics.
+
+2013 - 2014	MALDIVES NATIONAL UNIVERSITY, Malé Republic of Maldives.
+Advanced Certificate in Information Technology.
+
+High Distinction in Fundamentals of Programming.
+High Distinction in Application Development. 
+Distinction in Computer Concepts and PC Application
+Distinction in English for further Studies.
+
+1994 - 2004	AHMADIYYA INTERNATIONAL SCHOOL, Malé Republic of Maldives.
+IGCSE, GCE and SSC
+Principles of Accounts 	A	Islamic Studies 	B
+Commerce 	A	Economics	C
+Fisheries Science	B	Dhivehi Language 	C
+Mathematics	B		
+.
+Additional skills
+	Software Development, Graphic Designing, Writer, fluent in Arabic keyboard, drawing 3D images and floor plans.
+
+Driving License: A0, A1, B1, C1
+
+References
+	Mohamed Rabeeu, Council Executive of Thiladhunmathee Dhekunu Buree Vaikaradhoo Council Ge Idhaaraa
++960 99004522, mohamed.rabeeu@vaikaradhoo.gov.mv
+
+Hawwa Latheef, Vice rector for the Academic of Collage of Islamic Studies
+(+960) 7907701, hawwalathyf@gmail.com
+
+
+
+
+
+
+1 - Hussain Shareef_CV.pdf                15 - Other Academic Documents.pdf         6 - Work Experience - MACL.pdf
+2 - ID Card.pdf  
+
+10 - ChinaImport.pdf                      16 - Driving Experience.pdf               7 - Work Experience - Other Offices.pdf
+11 - Public Procurement.pdf                                        8 - Annex 6 - Iqraaru.pdf
+12 - Maalee&Viya.pdf                      3 - Diploma in IT.pdf                     9 - Enterprenurship.pdf
+13 - Air-side Experience Documents.pdf    4 - Work Experience Letter from SVC.pdf
+14 - CISCO Certificate.pdf                5 - Reference Letter - Ismail Ibrahim.pdf
+
+
+
+
+*/
+
+
+
+
+/*
+
+Date: 22 Feb 2021
+To Whom It Concern
+I understand that Mr Hussain Shareef (National ID A147829) is exploring employment options and has asked me to support his application to this end.   Having worked with Mr Shareef since November 2018 when I started my assignment as Project Director for the New Passenger Terminal Project at MACL, until June 2020, I am very well acquainted with many aspects of his competencies and overall work ethics.
+
+While Mr Shareef’s core skill may be Information Technology, my interactions with him lead me to assess that this is only one aspect of other very strong skills he possesses and which will be of immense use in a project or general management organization. In addition to his primary duties of IT and admin support to the project office as Officer, Administrative, he has often been tasked with provide project management related assistance by the team in the Project Office. I am inclined to make special mention of his strong aptitude for document management and control – especially given how systematic he was in handling this aspect in an office with as large a project document control work load as the MACL Projects Office. He is also equally very competent in handling project logistics and can very ably assist in tracking and administering project finances.
+
+A very committed and dedicated team player, Shareef is very capable of working independently and has the maturity to prioritize tasks in a high demanding organization. I believe Mr Shareef would be an asset to any organization and am pleased to recommend him for a project or general management position.  Please do not hesitate to contact me for any clarification.
+
+Yours sincerely,    
+Ismail Ibrahim Project Director Passenger Terminal Project Velana International Airport Upgrading Program Maldives
+Tel: +960 797 2851 
+Email: Ismail.ibrahim@imd.mv 
+LinkedIn: https://www.linkedin.com/in/ismail-ibrahim-14a15a16 
+
+
+*/
